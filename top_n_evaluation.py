bd73b47bd000da64fafda561f19a58e6e0f9b027
Karolina-Lach <carolina.lach@gmail.com>
2022-06-21 03:21:46 +0200

    Tfrs evaluation

--- top_n_evaluation.py
+++ top_n_evaluation.py
@@ -28,6 +28,56 @@ import heapq
 
 
 
+def create_anti_testset_for_user_tfrs(user_id, 
+                                 user_items_in_trainset,
+                                 ratings_df, 
+                                 sample_size=500, 
+                                 user_sample_size=0,
+                                 knn=False):
+    # get all items rated by user
+    user_items = ratings_df[ratings_df.AuthorId == user_id][['RecipeId', 'Rating']]
+    # get all items rated by user that are NOT in the trainset
+    candidates = user_items[~user_items.RecipeId.isin(user_items_in_trainset)].RecipeId.unique()
+    
+    # all items in ratings that are NOT rated by user
+    all_items = ratings_df[~ratings_df.RecipeId.isin(user_items_in_trainset)]
+    all_items = all_items[~all_items.RecipeId.isin(candidates)].RecipeId.unique()
+
+    if user_sample_size > 0:
+        candidates = np.random.choice(candidates, size=min(user_sample_size, len(candidates)))
+    else:
+        if (len(candidates) >= sample_size):
+    #         print(len(user_items_in_trainset))
+            candidates = np.random.choice(candidates, size=min(len(candidates), int(sample_size*0.5)))
+        
+#     number_of_rated_items = len(set(candidates).intersection(user_items.index))
+    sample_size = sample_size - len(candidates)
+
+    items_sample = np.random.choice(all_items, size=sample_size)
+    fill = ratings_df.Rating.mean()
+    
+    anti_testset = []
+    relevant_items = []
+    for item_id in items_sample:
+        if knn:
+            anti_testset.append(item_id)
+        else:
+            anti_testset.append((user_id, item_id, fill))
+
+    for item_id in candidates:
+        if knn:
+            anti_testset.append(item_id)
+        else:
+            anti_testset.append((user_id, item_id, fill))
+        relevant_items.append(item_id)
+    
+    random.shuffle(anti_testset)
+
+    return anti_testset, relevant_items
+
+
+
+
 def create_anti_testset_for_user_all(user_id, user_items_in_trainset, ratings_df, knn=False):
     
     # get all user items
@@ -184,3 +234,38 @@ def create_recommendation_top_n_evaluation(train_df,
         i += 1
         
     return recommendations, relevant_items
+
+
+
+
+def create_anti_testset_for_user_all_tfrs(user_id, user_items_in_trainset, ratings_df, knn=False):
+    
+    # get all user items
+    user_items = ratings_df[ratings_df.AuthorId == user_id][['RecipeId', 'Rating']].set_index("RecipeId")
+    # all items used in ratings
+    all_items = ratings_df.RecipeId.unique()
+    # get mean rating
+    fill = ratings_df.Rating.mean()
+
+    # print(f"All items {len(all_items)}")
+    # print(f"All items in trainset {len(user_items_in_trainset)}")
+    anti_testset = []
+    relevant_items = []
+    for item_id in all_items:
+        if item_id not in user_items_in_trainset:
+            if knn:
+                if item_id in user_items.index:
+                    # print('x')
+                    anti_testset.append(item_id)
+                    relevant_items.append(item_id)
+                else:
+                    anti_testset.append(item_id)
+            else:    
+                if item_id in user_items.index:
+                    # print('x')
+                    anti_testset.append((user_id, item_id, user_items.loc[item_id, 'Rating']))
+                else:
+                    anti_testset.append((user_id, item_id, fill))
+        
+
+    return anti_testset, relevant_items
